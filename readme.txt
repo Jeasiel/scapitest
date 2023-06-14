5b7cde08be4d2276237ddc3f69dbf2b335480b13
jeasiel <jeasielabner@gmail.com>
2023-06-14 16:59:03 -0300

    update

---
+++ readme.txt
@@ -0,0 +1,8 @@
+Para fazer:
+- lista de objetos com nome e imagem da carta para cada carta de um booster (15 cartas)
+- fazer uma seleção de cartas aleatórias de cada raridade, na versão dos booster
+- fazer um "dropdown" com todas as versões "prontas" <- talvez não agora
+- processar a geração de boosters na velocidade correta, com o delay
+- mostrar tudo na ordem:
+    opções - boosters + terrenos básicos + cmc + número de cartas em cada cmc - confirmação de que terminou
+    - mostrar lista do deck e botão de reset
--- selado/script.js
+++ selado/script.js
@@ -1,6 +1,36 @@
+const setField = document.getElementById("set");
+let passed = false;
 
+async function checkSet(code){
+    if(code == "" || code.length != 3){
+        setField.style.outlineColor = "red";
+        setField.focus();
+    } else if(!passed) {
+        try{
+            passed = true;
+            await wait(150);
+            const response = await fetch("https://api.scryfall.com/sets/" + code);
+            const data = await response.json();
+            console.log(data);
+            setField.style.outlineColor = "black";
+            if (data["status"] != undefined){
+                setField.style.outlineColor = "red";
+                setField.focus();
+                console.error("Erro! " + data["status"]);
+                passed = false;
+            } else {
+                const confirm = document.createElement("p");
+                confirm.innerText = "Tudo certo! Vamos ao próximo passo.";
+                document.getElementById("options").appendChild(confirm);
+                passed = true;
+            }
+        } catch(error){
+            console.error(error);
+        }
+    }
+}
 
-async function getRandomCardByTag(tag){
+async function getRandomCard(){
     try{
         const response = await fetch("https://api.scryfall.com/cards/random");
         const data = await response.json();
@@ -8,4 +38,10 @@ async function getRandomCardByTag(tag){
     } catch(error){
         console.error(error);
     }
+}
+
+function wait(time) {
+    return new Promise(resolve => {
+        setTimeout(resolve, time);
+    });
 }
