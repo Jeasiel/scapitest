c049b720af0f1416383ffa4280a229822b54aad6
jeasiel <jeasielabner@gmail.com>
2023-06-14 11:08:30 -0300

    update

--- seladoscript.js
+++ seladoscript.js
@@ -1,6 +1,8 @@
+
+
 async function getRandomCardByTag(tag){
     try{
-        const response = await fetch("https://api.scryfall.com/cards/random?q=art:jhoira");
+        const response = await fetch("https://api.scryfall.com/cards/random");
         const data = await response.json();
         console.log(data);
     } catch(error){
